ca04f47aedb9e56a7e3933df66b8de62b621ba88
Mark Edosa <maqui7295@gmail.com>
2020-08-24 08:38:08 +0100

    chore!: project setup started

--- babel-node.config.js
+++ babel-node.config.js
@@ -10,5 +10,14 @@ module.exports = {
       },
     ],
   ],
-  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    [
+      '@babel/plugin-proposal-private-methods',
+      {
+        loose: true,
+      },
+    ],
+    ['@babel/plugin-proposal-optional-chaining']
+  ],
 };
